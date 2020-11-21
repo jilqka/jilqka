7d552bf1cd19076dbcfc2fa27c5174a205395263
jilqka <72138367+jilqka@users.noreply.github.com>
2020-11-21 22:43:30 +0200

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,22 @@
+public class Circle {
+    private double radius;
+    public Circle(){
+        radius=1;
+    }
+    public Circle(double newRadius){
+        radius=newRadius;
+    }
+    public double getArea(){
+        return Math.PI*radius*radius;
+    }
+    public double getPerimeter(){
+        return Math.PI*radius*2;
+    }
+    public void setRadius(double newRadius){
+        this.radius=newRadius;
+    }
+    public double getRadius(){
+       return radius;
+    }
+
+}
