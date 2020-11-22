5340d69a643cbd48aa7c10297778981332feb0d8
jilqka <72138367+jilqka@users.noreply.github.com>
2020-11-22 16:28:41 +0200

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,61 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double yearInterestRate;
+    public Account(){
+        this.id=0;
+        this.balance=0;
+        this.yearInterestRate=0;
+    }
+    public Account(int id,double balance){
+        this.id=id;
+        this.balance=balance;
+        this.yearInterestRate=0;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getYearInterestRate() {
+        return yearInterestRate;
+    }
+
+    public void setYearInterestRate(double yearInterestRate) {
+        this.yearInterestRate = yearInterestRate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    public void withdraw(double withdraw){
+        setBalance(this.balance-withdraw);
+
+    }
+    public void deposit(double deposit){
+       setBalance(this.balance+deposit);
+    }
+    public double getMonthlyInterestRate(){
+        return yearInterestRate/12;
+    }
+    public double getMonthyInterest(){
+        return getMonthlyInterestRate()*balance;
+    }
+    public void setDate() throws ParseException {
+        Date date=new SimpleDateFormat("dd/MM/yyyy").parse("22/11/2020");
+        System.out.println("Date: "+date);
+    }
+
+}
