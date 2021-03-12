03637763c910e7b7fb10592a1a73412d8893ef62
jilqka <72138367+jilqka@users.noreply.github.com>
2021-03-12 09:31:38 +0200

    Add files via upload

--- Generic_sort.java
+++ Generic_sort.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Generic_sort {
     public static void main(String[] args) {
         GeometricObject[] geometricArr = {new Circle(4), new Circle(2), new Rectangle(5,4)};
@@ -33,5 +35,44 @@ public class Generic_sort {
             System.out.print(list[i] + " ");
         System.out.println();
     }
+    public static <E> void shuffle(ArrayList<E> list){
+        for (int i = 0;  i<list.size() ; i++) {
+            int randomIndex=(int) (Math.random()*list.size());
+            E temp= list.get(i);
+            list.set(i, list.get(randomIndex));
+            list.set(randomIndex,temp);
+
+        }
+    }
+    public static <E extends Comparable<E>> void sort(ArrayList<E> list){
+        E currentMin;
+        int currentMinIndex;
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            currentMin = list.get(i);
+            currentMinIndex = i;
+            for (int j = i + 1; j < list.size(); j++) {
+                if (currentMin.compareTo(list.get(j)) > 0) {
+                    currentMin = list.get(j);
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list.set(currentMinIndex,list.get(i));
+                list.set(i,currentMin);
+            }
+        }
+    }
+    public static <E extends Comparable<E>> E min(ArrayList<E> list){
+        E currentMin=list.get(0);
+
+        for (int i = 0; i < list.size() - 1; i++) {
+            if (currentMin.compareTo(list.get(i))>0) {
+                currentMin=list.get(i);
+            }
+        }
+        return currentMin;
+    }
+
 
 }
