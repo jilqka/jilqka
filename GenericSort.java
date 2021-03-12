e769f558480d363f5c8d5e927a8d1b78cee3bcbd
jilqka <72138367+jilqka@users.noreply.github.com>
2021-03-12 09:33:22 +0200

    Add files via upload

---
+++ GenericSort.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+
+public class GenericSort {
+    public static void main(String[] args) {
+        GeometricObject[] geometricArr = {new Circle(4), new Circle(2), new Rectangle(5, 4)};
+        System.out.println("Before sort:\n");
+        printList(geometricArr);
+        sort(geometricArr);
+        System.out.println("Sorted: \n");
+        printList(geometricArr);
+    }
+
+    public static <E extends Comparable<E>> void sort(E[] list) {
+        E currentMin;
+        int currentMinIndex;
+
+        for (int i = 0; i < list.length - 1; i++) {
+            currentMin = list[i];
+            currentMinIndex = i;
+            for (int j = i + 1; j < list.length; j++) {
+                if (currentMin.compareTo(list[j]) > 0) {
+                    currentMin = list[j];
+                    currentMinIndex = j;
+                }
+            }
+            if (currentMinIndex != i) {
+                list[currentMinIndex] = list[i];
+                list[i] = currentMin;
+            }
+        }
+    }
+
+    public static void printList(Object[] list) {
+        for (int i = 0; i < list.length; i++)
+            System.out.print(list[i] + " ");
+        System.out.println();
+    }
+
+
+}
+
